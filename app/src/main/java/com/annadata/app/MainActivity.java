e80f374c76a953c79884c615494747e8862ccec4
Harshit Singhal <harshitsinghal0919@gmail.com>
2024-09-08 14:06:29 +0530

    Updated

--- app/src/main/java/com/annadata/app/MainActivity.java
+++ app/src/main/java/com/annadata/app/MainActivity.java
@@ -1,6 +1,12 @@
 package com.annadata.app;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +14,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private EditText phoneNumberEditText;
+    private Button sendOtpButton;
+    private FirebaseAuth mAuth;
+    private String phoneNumber;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +40,61 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        phoneNumberEditText = findViewById(R.id.phoneNumberEditText);
+        sendOtpButton = findViewById(R.id.sendOtpButton);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        sendOtpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                phoneNumber = "+91"+phoneNumberEditText.getText().toString().trim();
+
+                if (!phoneNumber.isEmpty()) {
+                    if (phoneNumber.length() == 10) {
+                        sendVerificationCode("+91" + phoneNumber); // Assuming country code is +91 for India
+                    } else {
+                        Toast.makeText(MainActivity.this, "Please enter a valid phone number", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Phone number can't be empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
+
+    private void sendVerificationCode(String phoneNumber) {
+        PhoneAuthOptions options = PhoneAuthOptions.newBuilder(mAuth)
+                .setPhoneNumber(phoneNumber)
+                .setTimeout(60L, TimeUnit.SECONDS)
+                .setActivity(this)
+                .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(com.google.firebase.auth.PhoneAuthCredential credential) {
+                        // Auto-verification or instant verification
+                    }
+
+                    @Override
+                    public void onVerificationFailed(com.google.firebase.FirebaseException e) {
+                        Toast.makeText(MainActivity.this, "Verification failed", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCodeSent(String verificationId, PhoneAuthProvider.ForceResendingToken token) {
+                        super.onCodeSent(verificationId, token);
+                        Intent intent = new Intent(MainActivity.this, verifyOTPActivity.class);
+                        intent.putExtra("verificationId", verificationId);
+                        startActivity(intent);
+                    }
+                })
+                .build();
+
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+
 }
